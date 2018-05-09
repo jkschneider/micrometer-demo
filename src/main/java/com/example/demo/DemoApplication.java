c37c2ce6b15b3315067b926efa103517c5e6750a
Jon Schneider <jschneider@pivotal.io>
2018-05-08 22:24:01 -0500

    Work on dashboard

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +22,13 @@ public class DemoApplication {
 class TeamsController {
 	// sorted in descending order of importance, of course
 	@GetMapping("/teams")
-	public List<String> teams() {
-		return Arrays.asList("St. Louis Cardinals", "New York Yankees", "Chicago Cubs");
+	public Flux<String> teams() {
+		return Flux.just("St. Louis Cardinals", "New York Yankees", "Chicago Cubs");
+	}
+
+	// sorted in descending order of importance, of course
+	@GetMapping("/teams/{name}")
+	public Mono<String> teams(@PathVariable String name) {
+		return Mono.just("about this team...");
 	}
 }
